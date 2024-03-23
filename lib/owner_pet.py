7a4817e916129b8c4b36143022f9b6269d4f8a57
RumiYo <yokoyamarumi@gmail.com>
2024-03-23 13:44:31 -0400

    Complete lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,41 @@
 class Pet:
-    pass
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=""):
+        self.name = name
+        self.owner = owner
+        self.pet_type = pet_type
+        Pet.add_pet_to_all(self)
+
+    @property 
+    def pet_type(self):
+        return self._pet_type
+    
+    @pet_type.setter
+    def pet_type(self, value):
+        if not value in Pet.PET_TYPES:
+            raise Exception
+        self._pet_type = value
+
+    @classmethod
+    def add_pet_to_all(cls, pet):
+        cls.all.append(pet)
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        owners_pet = Owner.pets(self)
+        return sorted(owners_pet, key=lambda pet: pet.name)
